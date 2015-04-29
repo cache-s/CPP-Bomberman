3bfc52e93af522059c8bb71ce82bbaa5714569bf
Mathieu <mathieu.bourmaud@epitech.eu>
2015-04-29 11:03:18 +0200

    base

---
+++ sources/Main.cpp
@@ -0,0 +1,16 @@
+//
+// Main.cpp for  in /home/bourma_m/Projet/C++/cpp_bomberman/sources
+// 
+// Made by Mathieu Bourmaud
+// Login   <bourma_m@epitech.net>
+// 
+// Started on  Wed Apr 29 10:58:26 2015 Mathieu Bourmaud
+// Last update Wed Apr 29 10:59:54 2015 Mathieu Bourmaud
+//
+
+int			main(int ac, char **av)
+{
+  (void)ac;
+  (void)av;
+  return (0);
+}
