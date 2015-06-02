0a056e33250e4ad01b984c8b2a41d82ee6cc028f
porres_m <porres_m@epitech.eu>
2015-06-02 18:20:10 +0200

    EventManager updated

--- sources/Core/Core.cpp
+++ sources/Core/Core.cpp
@@ -5,14 +5,14 @@
 // Login   <porres_m@epitech.net>
 // 
 // Started on  Sun May 24 18:01:57 2015 Martin Porrès
-// Last update Wed May 27 16:51:47 2015 Martin Porrès
+// Last update Tue Jun  2 18:14:45 2015 Martin Porrès
 //
 
 #include	"Core.hpp"
 
 Core::Core(IGUI &gui) : _gui(&gui)
 {
-
+  //generate map / load map
 }
 
 Core::~Core(void)
