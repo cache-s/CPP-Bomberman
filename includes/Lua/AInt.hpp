35a66114333304618481681373f7e9963bb23c9d
charie_p <pierre.charie@epitech.eu>
2015-06-12 12:22:20 +0200

    dada

---
+++ includes/Lua/AInt.hpp
@@ -0,0 +1,33 @@
+//
+// AI.hpp for  in /home/charie_p/rendu/cpp_bomberman/includes/Lua
+//
+// Made by Pierre Charie
+// Login   <charie_p@epitech.net>
+//
+// Started on  Wed Jun 10 17:13:21 2015 Pierre Charie
+// Last update Fri Jun 12 11:54:04 2015 Pierre Charie
+//
+
+#ifndef		AI_HPP
+# define	AI_HPP
+
+#include	<sstream>
+
+# include       "Bomberman.hpp"
+# include	"IEntity.hpp"
+
+class		AInt
+{
+public:
+  AInt(int, int);
+  ~AInt();
+  void		move(std::string, int, int, int, int);
+private:
+
+  int		_width;
+  int		_height;
+
+  std::string   mapMerge(std::map<std::pair<int, int>, IEntity<glm::vec3> *> playerMap, std::map<std::pair<int, int>, IEntity<glm::vec3> *> gameMap);
+};
+
+#endif
