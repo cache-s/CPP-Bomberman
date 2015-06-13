e7455132e35bac1fa288b5f207f16c26f8ec97e3
porres_m <porres_m@epitech.eu>
2015-06-13 11:54:30 +0200

    Merci charie_p

---
+++ includes/Entity/eKey.hpp
@@ -0,0 +1,20 @@
+#ifndef		_EKEY_HPP_
+# define	_EKEY_HPP_
+
+enum		eKey
+  {
+    UP1 = 200,
+    DOWN1 = 201,
+    LEFT1 = 202,
+    RIGHT1 = 203,
+    BOMB1 = 204,
+    UP2 = 205,
+    DOWN2 = 206,
+    LEFT2 = 207,
+    RIGHT2 = 208,
+    BOMB2 = 209,
+    QUIT = 210,
+    NONE = 211,
+  };
+
+#endif
